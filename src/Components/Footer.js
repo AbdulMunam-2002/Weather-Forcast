3ceaf0bdbbf507379c749f46150898ae75cf1ab6
Jaweriasaleem54 <125985442+Jaweriasaleem54@users.noreply.github.com>
2023-12-06 15:18:53 +0500

    footer updated

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,8 +1,27 @@
+import React from 'react';
+import { Navbar, Container, Nav, NavDropdown, Button } from 'react-bootstrap';
+import additionalImage from '../Assets/Images/Header1.png';
+import Image2 from '../Assets/Img 1.png';
+
 export default function Footer()
 {
+    const headerStyle = {
+        backgroundImage: `url(${additionalImage})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center center',
+        minHeight: '75px',
+      };
     return(
-        <>
-            <h1>Footer</h1>
+        <>      <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary" style={headerStyle}>
+        <Container>
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mx-auto">
+              <Nav.Link href="#features">© 2022 — 2023 SKYSYNC ® All rights reserved</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
         </>
     )
 }
