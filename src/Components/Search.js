c8177caa3bcd11d28a8ea2149bee404d958d0742
AbdulMunam-2002 <abdulmunam666@gmail.com>
2023-12-19 16:43:24 +0500

    Final Project

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -42,35 +42,9 @@ export default function Search() {
     <>
       <div className='bg'>
         <Header />
-        <div className='container'>
-          <div className='row'>
-            <div className='col-lg-6' style={{ maxWidth: '400px', marginTop: '190px' }}>
-              <input
-                type="search"
-                className="form-control"
-                name="search"
-                id="search"
-                placeholder="Search for a place"
-                required
-                style={{ borderRadius: '10px', backgroundColor: '#FFFFFF' }}
-              />
-            </div>
-            <div className='col-lg-2' style={{ marginTop: '190px' }}>
-            <Link to='/Weather'>
-              <Button type="submit" style={buttonStyle} onClick={handleSearch}>
-                Get Weather
-          </Button>
-          </Link>  
-            </div>
-          </div>
-        </div>  
-        <br />
-        <br />
-        <br />
-        <br />
 
         <iframe
-          style={{ border: '0', width: '100%', height: '250px', marginBottom:'50px' }}
+          style={{ border: '0', width: '100%', height: '250px', marginTop:'150px' }}
           src={`https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d${position[1]}!3d${position[0]}!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621`}
           frameBorder="10px"
           allowFullScreen
